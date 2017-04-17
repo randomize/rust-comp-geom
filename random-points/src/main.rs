2c629d2dc28a262594b30c5f4dbe98face54b322
Eugene Mihailenco <mihailencoe@gmail.com>
2017-04-17 22:10:12 +0300

    Using Rmath in first sample

--- random-points/src/main.rs
+++ random-points/src/main.rs
@@ -1,8 +1,25 @@
 
 extern crate piston_window;
+extern crate rand;
+extern crate rmath;
 
+use rmath::vector::Vector2D;
+use rmath::vector::gen_random_points;
+use rand::Rng;
 
 fn main() {
-    println!("Hello, world!");
-}
 
+    let mut rng = rand::thread_rng();
+
+    let a: Vector2D = rng.gen();
+    let b: Vector2D = rng.gen();
+
+    println!("a: {}, b: {}", a, b);
+
+    let ar = gen_random_points(&mut rng, 10);
+
+    for (i, p) in ar.iter().enumerate() {
+        println!("{}:{}", i, p);
+    }
+
+}
