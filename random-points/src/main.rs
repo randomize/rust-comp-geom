6e3c94e4905e952fde4b8c14f12948b5c8db1347
Eugene Mihailenco <mihailencoe@gmail.com>
2017-04-20 22:00:15 +0300

    Random points example done

--- random-points/src/main.rs
+++ random-points/src/main.rs
@@ -6,9 +6,13 @@ extern crate rmath;
 use rmath::vector::Vector2D;
 use rmath::vector::gen_random_points;
 use rand::Rng;
+use piston_window::*;
+
+
 
 fn main() {
 
+    let black = [0.0, 0.0, 0.0, 1.0];
     let mut rng = rand::thread_rng();
 
     let a: Vector2D = rng.gen();
@@ -16,10 +20,35 @@ fn main() {
 
     println!("a: {}, b: {}", a, b);
 
-    let ar = gen_random_points(&mut rng, 10);
+    let ar = gen_random_points(&mut rng, 100);
 
+    /*
     for (i, p) in ar.iter().enumerate() {
-        println!("{}:{}", i, p);
+        println!("{}:{}", i, p.x);
     }
+    */
+
+    let height = 500;
+    let width = 500;
+    let dot_size = 3.0;
+
+    let mut window: PistonWindow =
+        WindowSettings::new("Random dots", [width, height])
+        .exit_on_esc(true).build().unwrap();
+
+    while let Some(e) = window.next() {
+        window.draw_2d(&e, |c, g| {
+            clear([1.0; 4], g);
+            rectangle([1.0, 0.0, 0.0, 1.0], // red
+                      [0.0, 0.0, 100.0, 100.0],
+                      c.transform, g);
+            for p in ar.iter() {
+                let l = p.len();
+                ellipse(black, [p.x * width as f64, p.y * width as f64, dot_size, dot_size],
+                   c.transform, g);
+            }
+        });
+    }
+
 
 }
