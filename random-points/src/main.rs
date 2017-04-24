bc67eac5fa5bc3077be17904bb257d240e795bd7
Eugene Mihailenco <mihailencoe@gmail.com>
2017-04-24 12:38:04 +0300

    Little cleanup and refactoring

--- random-points/src/main.rs
+++ random-points/src/main.rs
@@ -1,11 +1,10 @@
+// simple example that just draws random points in range 0..1
 
 extern crate piston_window;
 extern crate rand;
 extern crate rmath;
 
-use rmath::vector::Vector2D;
 use rmath::alg::gen_random_points;
-use rand::Rng;
 use piston_window::*;
 
 
@@ -13,38 +12,31 @@ use piston_window::*;
 fn main() {
 
     let black = [0.0, 0.0, 0.0, 1.0];
-    let mut rng = rand::thread_rng();
-
-    let a: Vector2D = rng.gen();
-    let b: Vector2D = rng.gen();
-
-    println!("a: {}, b: {}", a, b);
-
-    let ar = gen_random_points(&mut rng, 100);
-
-    /*
-    for (i, p) in ar.iter().enumerate() {
-        println!("{}:{}", i, p.x);
-    }
-    */
-
     let height = 500;
     let width = 500;
     let dot_size = 3.0;
+    let count = 500;
 
-    let mut window: PistonWindow =
-        WindowSettings::new("Random dots", [width, height])
-        .exit_on_esc(true).build().unwrap();
+    let mut rng = rand::thread_rng();
+
+    let ar = gen_random_points(&mut rng, count);
+
+
+    let opengl = OpenGL::V3_3;
+    let mut window: PistonWindow = WindowSettings::new("Random dots", [width, height])
+        .exit_on_esc(true)
+        .opengl(opengl)
+        .samples(4)
+        .vsync(true)
+        .build()
+        .unwrap();
 
     while let Some(e) = window.next() {
         window.draw_2d(&e, |c, g| {
             clear([1.0; 4], g);
-            rectangle([1.0, 0.0, 0.0, 1.0], // red
-                      [0.0, 0.0, 100.0, 100.0],
-                      c.transform, g);
-            for p in ar.iter() {
-                ellipse(black, [p.x * width as f64, p.y * width as f64, dot_size, dot_size],
-                        c.transform, g);
+            for p in &ar {
+                let e = [p.x * width as f64, p.y * width as f64, dot_size, dot_size];
+                ellipse(black, e, c.transform, g);
             }
         });
     }
